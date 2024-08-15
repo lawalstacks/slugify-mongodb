c27e4bebcbda81354f1722ad42b7429377c61cb6
Kazeem lawal <49644714+lawalstacks@users.noreply.github.com>
2024-08-15 17:16:35 +0100

    Update new features: options for slugify.mjs

--- src/slugify.mjs
+++ src/slugify.mjs
@@ -5,13 +5,28 @@
  * @param {string} text - The text to be slugified.
  * @returns {string} - The slugified text.
  */
-export const slugify = (text) => {
-    return text
-        .toString()
-        .toLowerCase()
-        .trim()
-        .replace(/[\s\W]+/g, '-')
-        .replace(/^-+|-+$/g, '');
+
+export const slugify = (text, options = {}) => {
+  const {
+    separator = '-',
+    lower = true,
+    preserveCase = false,
+    maxLength = Infinity,
+    reservedWords = [],
+  } = options;
+
+  let slug = text.toString().trim()
+    .replace(/[\s\W]+/g, separator)
+    .replace(new RegExp(`^${separator}+|${separator}+$`, 'g'), '');
+
+  if (lower && !preserveCase) slug = slug.toLowerCase();
+  slug = slug.slice(0, maxLength);
+
+  if (reservedWords.includes(slug)) {
+    slug += `-${Date.now()}`;
+  }
+
+  return slug;
 };
 
 /**
@@ -20,18 +35,19 @@ export const slugify = (text) => {
  * @param {Model} Model - The Mongoose model to check for uniqueness.
  * @returns {Promise<string>} - The unique slug.
  */
-export const generateUniqueSlug = async (text, Model) => {
-    let slug = slugify(text);
-    let uniqueSlug = slug;
-    let count = 1;
 
-	if(!Model){
-		console.error({"error":"generateUniqueSlug(string,Model:mongodb model- func params not defined!)"});                       return;
-	}
+export const generateUniqueSlug = async (text, Model, options = {}) => {
+  let slug = slugify(text, options);
+  let uniqueSlug = slug;
+  let count = 1;
+if(!Model){
+		console.error({"error":"generateUniqueSlug(string,Model:mongodb model- func params not defined!)"});
+return;
 
-while (await Model.findOne({ slug: uniqueSlug })) {
-        uniqueSlug = `${slug}-${count}`;
-        count++;
-    }
-    return uniqueSlug;
+  while (await Model.findOne({ slug: uniqueSlug })) {
+    uniqueSlug = `${slug}-${count}`;
+    count++;
+  }
+
+  return uniqueSlug;
 };
